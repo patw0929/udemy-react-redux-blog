6b03d14b82be6f737d2b59b303c2cb99809c8bfe
patw <patw.hi@gmail.com>
2016-06-14 16:14:38 +0800

    React-router and index route

---
+++ src/components/posts_index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PostsIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}
+      routes={routes} />
   </Provider>
   , document.querySelector('.container'));
