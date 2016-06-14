3a4673300379b59f46d395997e63580f369355c6
patw <patw.hi@gmail.com>
2016-06-14 16:55:20 +0800

    Refactor posts index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
@@ -21,10 +20,4 @@ class PostsIndex extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    fetchPosts,
-  }, dispatch);
-}
-
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, { fetchPosts })(PostsIndex);
