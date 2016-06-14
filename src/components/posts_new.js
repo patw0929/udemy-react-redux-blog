013ddac23a7c4145bc6eae6865b2ceea64407fcf
patw <patw.hi@gmail.com>
2016-06-14 17:33:08 +0800

    Form fields and redux form binding

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   constructor(props) {
@@ -6,9 +7,28 @@ class PostsNew extends Component {
   }
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create a new post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content'],
+})(PostsNew);
