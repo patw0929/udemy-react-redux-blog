642c42feedc6b46fe07d17848ceda117410a2881
patw <patw.hi@gmail.com>
2016-06-14 16:17:01 +0800

    Using redux-promise as middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import ReduxPromose from 'redux-promise';
 import routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  ReduxPromise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
