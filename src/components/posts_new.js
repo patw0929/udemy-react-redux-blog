53d31334eecb4b0f152281179f0f725b001bac80
patw <patw.hi@gmail.com>
2016-06-14 17:45:54 +0800

    Binding form fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -6,20 +6,29 @@ class PostsNew extends Component {
     super(props);
   }
   render() {
+    const {
+      fields: {
+        title,
+        categories,
+        content,
+      },
+      handleSubmit,
+    } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
