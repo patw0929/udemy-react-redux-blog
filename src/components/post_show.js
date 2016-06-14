288a272d79a2aca96944c83078b84ceb0a249b4e
patw <patw.hi@gmail.com>
2016-06-14 18:49:18 +0800

    PostShow component & route

---
+++ src/components/post_show.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>PostShow</div>
+    );
+  }
+}
+
+export default PostShow;
