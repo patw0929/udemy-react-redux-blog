8115b9f0648d731f31d6750ec769ec1e45c02bbf
patw <patw.hi@gmail.com>
2016-06-14 19:36:24 +0800

    Webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: './dist/',
+    publicPath: '/udemy-react-redux-blog/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template : './index.html',
+      hash     : false,
+      filename : 'index.html',
+      inject   : false,
+      outputPath: '/udemy-react-redux-blog/',
+    }),
+  ],
+};
