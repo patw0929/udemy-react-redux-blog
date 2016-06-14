eaf719e87e8027f4f612319d0bfddfd7efb36a80
patw <patw.hi@gmail.com>
2016-06-14 16:58:02 +0800

    PostsNew component and route

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Create Form</div>
+    );
+  }
+}
+
+export default PostsNew;
