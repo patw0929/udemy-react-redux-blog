49aaad9939f3645b109cd37365cf65cb2fd3a97e
patw <patw.hi@gmail.com>
2016-06-14 19:07:54 +0800

    PostShow component display content

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { readPost } from '../actions/index';
 
 class PostShow extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentWillMount() {
+    this.props.readPost(this.props.params.id);
+  }
+
   render() {
+    if (!this.props.post) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, categories, content } = this.props.post;
+
     return (
-      <div>PostShow</div>
+      <div>
+        <Link to="/" className="btn btn-primary">Back to list</Link>
+
+        <h3>{title}</h3>
+
+        <h6>Categories: {categories}</h6>
+
+        <p>
+          {content}
+        </p>
+      </div>
     );
   }
 }
 
-export default PostShow;
+function mapStateToProps(state) {
+  return {
+    post: state.posts.post,
+  };
+}
+
+export default connect(mapStateToProps, { readPost })(PostShow);
